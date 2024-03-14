4a84a1aef62ebe8a3c4ca738e213ca8945701f0c
HeH-Gianni <gianni.tricarico@heh.be>
2024-03-14 10:47:52 +0100

    feat:Définition et utilisation de composants

---
+++ src/Components/Gallery.jsx
@@ -0,0 +1,11 @@
+import Photo from "./Photo";
+const Gallery = () => {
+  return (
+    <div>
+      <h1>Galerie de photos</h1>
+      <Photo />
+      <Photo />
+    </div>
+  );
+};
+export default Gallery;
---
+++ src/Components/Photo.jsx
@@ -0,0 +1,6 @@
+const Photo = () =>{
+    return(
+        <img src="https://picsum.photos/id/237/200/300" alt="" />
+    )
+}
+export default Photo;
